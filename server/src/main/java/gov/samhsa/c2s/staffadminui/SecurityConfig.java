98986bbc854e562e5b1ad747796e1cda2c4d752a
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-29 14:31:24 -0400

    Create initial spring boot project to server staff-admin-ui client
    #13210: As a developer, I will have implemented the Staff Admin UI for registering a patient

---
+++ server/src/main/java/gov/samhsa/c2s/staffadminui/SecurityConfig.java
@@ -0,0 +1,39 @@
+package gov.samhsa.c2s.staffadminui;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+public class SecurityConfig {
+    private static final String RESOURCE_ID = "staffAdminUI";
+
+    @Bean
+    public ResourceServerConfigurer resourceServer(SecurityProperties securityProperties) {
+        return new ResourceServerConfigurerAdapter() {
+            @Override
+            public void configure(ResourceServerSecurityConfigurer resources) {
+                resources.resourceId(RESOURCE_ID);
+            }
+
+            @Override
+            public void configure(HttpSecurity http) throws Exception {
+                if (securityProperties.isRequireSsl()) {
+                    http.requiresChannel().anyRequest().requiresSecure();
+                }
+                http.authorizeRequests()
+                        // Security scope for accessing management endpoint
+                        .antMatchers(HttpMethod.GET, "/management/**").access("#oauth2.hasScope('staffAdminUI.management')")
+                        .antMatchers(HttpMethod.POST, "/management/**").access("#oauth2.hasScope('staffAdminUI.management')")
+                        .antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('staffAdminUI.read')")
+                        .antMatchers(HttpMethod.OPTIONS, "/**").access("#oauth2.hasScope('staffAdminUI.read')")
+                        .anyRequest().denyAll();
+            }
+        };
+    }
+}
