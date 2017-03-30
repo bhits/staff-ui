56012f4a1c5a420b8254db91b47f74a98bd226eb
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-30 14:05:08 -0400

    Create initial page footer component
    #13210: As a developer, I will have implemented the Staff Admin UI for registering a patient

---
+++ client/src/app/layout/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'c2s-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -1,13 +1,20 @@
 import {NgModule} from "@angular/core";
 import {HeaderComponent} from "./header/header.component";
 import {SharedModule} from "../shared/shared.module";
+import {FooterComponent} from "./footer/footer.component";
 
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class LayoutModule {
 }
