f1264261426cb74743134da2d3e9eb963e95d1fb
JiahaoLi <Jiahao.Li@feisystems.com>
2017-04-03 16:23:23 -0400

    Implement HttpInterceptor Service
    #13210: As a developer, I will have implemented the Staff Admin UI for registering a patient

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,9 +1,23 @@
 import {NgModule} from "@angular/core";
 import {SecurityModule} from "../security/security.module";
+import {SlimLoadingBarModule, SlimLoadingBarService} from "ng2-slim-loading-bar";
+import {Http, RequestOptions, XHRBackend} from "@angular/http";
+import {httpInterceptorServiceFactory} from "./http-interceptor.service";
+import {TokenService} from "../security/shared/token.service";
 
 @NgModule({
-  imports: [SecurityModule],
+  imports: [
+    SecurityModule,
+    SlimLoadingBarModule.forRoot()
+  ],
   declarations: [],
+  providers: [
+    {
+      provide: Http,
+      useFactory: httpInterceptorServiceFactory,
+      deps: [XHRBackend, RequestOptions, SlimLoadingBarService, TokenService]
+    }
+  ],
   exports: []
 })
 export class CoreModule {
---
+++ client/src/app/core/http-interceptor.service.ts
@@ -0,0 +1,77 @@
+import {Injectable} from "@angular/core";
+import {
+  ConnectionBackend,
+  Headers,
+  Http,
+  Request,
+  RequestOptions,
+  RequestOptionsArgs,
+  Response,
+  XHRBackend
+} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {SlimLoadingBarService} from "ng2-slim-loading-bar";
+import {TokenService} from "../security/shared/token.service";
+
+@Injectable()
+export class HttpInterceptorService extends Http {
+  private UAA: string = "uaa";
+
+  constructor(backend: ConnectionBackend, defaultOptions: RequestOptions,
+              private slimLoadingBarService: SlimLoadingBarService,
+              private tokenService: TokenService) {
+    super(backend, defaultOptions);
+  }
+
+  request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+    this.slimLoadingBarService.start();
+    return this.intercept(super.request(url, this.setHeaders(options)));
+  }
+
+  get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.get(url, this.setHeaders(options)));
+  }
+
+  post(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+    if (url.indexOf(this.UAA) >= 0) {
+      return this.intercept(super.post(url, body, options));
+    } else {
+      return this.intercept(super.post(url, body, this.setHeaders(options)));
+    }
+  }
+
+  put(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.put(url, body, this.setHeaders(options)));
+  }
+
+  delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
+    return this.intercept(super.delete(url, this.setHeaders(options)));
+  }
+
+  intercept(observable: Observable<Response>): Observable<Response> {
+    return observable.do(() => this.slimLoadingBarService.complete());
+  }
+
+  private setHeaders(options: RequestOptionsArgs): RequestOptionsArgs {
+    if (!options) {
+      options = new RequestOptions({});
+    }
+
+    let token = this.tokenService.getOauthToken();
+
+    if (token && token['access_token']) {
+
+      if (!options.headers) {
+        options.headers = new Headers();
+      }
+      let access_token = token['access_token'];
+      options.headers.set('Authorization', 'Bearer ' + access_token);
+      options.headers.set('Content-Type', 'application/json');
+    }
+    return options;
+  }
+}
+
+export function httpInterceptorServiceFactory(xhrBackend: XHRBackend, requestOptions: RequestOptions, slimLoadingBarService: SlimLoadingBarService, tokenService: TokenService) {
+  return new HttpInterceptorService(xhrBackend, requestOptions, slimLoadingBarService, tokenService);
+}
