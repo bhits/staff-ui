62fbb56d74a6a5d3c4666c560f5dd3416be4393b
JiahaoLi <Jiahao.Li@feisystems.com>
2017-04-12 11:52:33 -0400

    Dynamically display total number of created patients
    #13210: As a developer, I will have implemented the Staff Admin UI for registering a patient

--- client/src/app/home/home/home.component.ts
+++ client/src/app/home/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {ApiUrlService} from "../../shared/api-url.service";
 import {UtilityService} from "../../shared/utility.service";
+import {PatientService} from "app/user/shared/patient.service";
 
 @Component({
   selector: 'c2s-home',
@@ -8,12 +9,24 @@ import {UtilityService} from "../../shared/utility.service";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public numberOfPatients: number = 0;
 
   constructor(private apiUrlService: ApiUrlService,
+              private patientService: PatientService,
               private utilityService: UtilityService) {
   }
 
   ngOnInit() {
+    const FIRST_PAGE: number = 0;
+    this.patientService.getPatients(FIRST_PAGE)
+      .subscribe(
+        (patients) => {
+          this.numberOfPatients = patients.totalElements
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
 
   navigateTo() {
