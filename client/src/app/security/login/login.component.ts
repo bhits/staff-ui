d70d9541cbaf23d0796ce689047338779ec4c96c
Teng Luo <Teng.Luo@feisystems.com>
2017-06-28 15:44:08 -0400

    Implement i18n on login component
    #14337 As a Developer, I will have moved all hard-coded user message on UI to a properties file

--- client/src/app/security/login/login.component.ts
+++ client/src/app/security/login/login.component.ts
@@ -1,7 +1,13 @@
 import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AuthenticationService} from "app/security/shared/authentication.service";
-
+import {ProfileService} from "../shared/profile.service";
+import {UtilityService} from "../../shared/utility.service";
+import {TokenService} from "../shared/token.service";
+import {CustomTranslateService} from "../../core/custom-translate.service";
+import {TranslateService} from "@ngx-translate/core";
+import {Profile} from "../../core/profile.model";
+import {UmsProfile} from "../shared/ums-profile.model";
 @Component({
   selector: 'c2s-login',
   templateUrl: './login.component.html',
@@ -13,7 +19,15 @@ export class LoginComponent implements OnInit {
   public unauthorized: boolean = false;
 
   constructor(private authenticationService: AuthenticationService,
+              private profileService: ProfileService,
+              private utilityService: UtilityService,
+              private tokenService: TokenService,
+              private customTranslateService: CustomTranslateService,
+              private translate: TranslateService,
               private formBuilder: FormBuilder) {
+    // Set default language for login page
+    const DEFAULT_LANGUAGE = "en";
+    translate.setDefaultLang(DEFAULT_LANGUAGE);
   }
 
   ngOnInit() {
@@ -31,7 +45,17 @@ export class LoginComponent implements OnInit {
       .subscribe(
         (res) => {
           this.unauthorized = false;
-          this.authenticationService.isLoggedIn(res);
+          this.authenticationService.onLoggedIn(res);
+          this.authenticationService.getUserProfile()
+            .subscribe(
+              (uaaProfile) => {
+                let profile = this.tokenService.createProfileObject(uaaProfile);
+                this.tokenService.storeUserProfile(profile);
+                this.getUMSProfileAndSetDefaultLanguage(profile);
+              }
+              ,
+              (error) => this.handleLoginError
+            );
         },
         err => {
           this.unauthorized = true;
@@ -39,6 +63,24 @@ export class LoginComponent implements OnInit {
           console.log(err);
         }
       );
+  }
+
+  getUMSProfileAndSetDefaultLanguage(uaaProfile: Profile) {
+    this.profileService.getUMSProfile().subscribe(
+      (profile: UmsProfile) => {
+        let localesCode: string[] = this.utilityService.getSupportedLocaleCode(profile.supportedLocales);
+        this.customTranslateService.addSupportedLanguages(localesCode);
+        this.customTranslateService.setDefaultLanguage(profile.userLocale);
+        this.profileService.setProfileInSessionStorage(profile);
+        this.authenticationService.onGetUserProfileSuccess(uaaProfile);
+      },
+      this.handleLoginError
+    )
+  }
+
+  handleLoginError(error: any) {
+    this.tokenService.deleteAccessToken();
+    this.unauthorized = true;
   }
 
   public getInputType(inputType: string) {
