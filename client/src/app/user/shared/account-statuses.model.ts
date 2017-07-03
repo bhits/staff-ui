a43ce13933e0612540c785f77c18d0fa7fc46463
Teng Luo <Teng.Luo@feisystems.com>
2017-07-03 09:24:32 -0400

    Implement i18n on account statues
    #14337 As a Developer, I will have moved all hard-coded user message on UI to a properties file

--- client/src/app/user/shared/account-statuses.model.ts
+++ client/src/app/user/shared/account-statuses.model.ts
@@ -7,9 +7,9 @@ export enum AccountStatus {
 
 export const ACCOUNT_STATUSES: Map<AccountStatus, string> = new Map(
   [
-    [AccountStatus.NotActivated, "Account Not Yet Activated"],
-    [AccountStatus.Verified, "Activation Email Sent"],
-    [AccountStatus.Activated, "Account Activated"],
-    [AccountStatus.Disabled, "Account Disabled"]
+    [AccountStatus.NotActivated, "USER.VERIFICATION.ACCOUNT_STATUSES.NOT_ACTIVATED_TEXT"],
+    [AccountStatus.Verified, "USER.VERIFICATION.ACCOUNT_STATUSES.VERIFIED_TEXT"],
+    [AccountStatus.Activated, "USER.VERIFICATION.ACCOUNT_STATUSES.ACTIVATED_TEXT"],
+    [AccountStatus.Disabled, "USER.VERIFICATION.ACCOUNT_STATUSES.DISABLED_TEXT"]
   ]
 );
