7171ad018de6edbb6cf9a05f19a7e6f1dddf027d
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-08-31 16:16:17 -0400

    Fix bug when user click on back button from home to login screen
    #15622 - Browser "back" button takes user to login page while still logged in

--- client/src/app/security/shared/can-activate-auth-guard.service.ts
+++ client/src/app/security/shared/can-activate-auth-guard.service.ts
@@ -3,13 +3,26 @@ import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapsh
 import {AuthorizationService} from "app/security/shared/authorization.service";
 import {UtilityService} from "app/shared/utility.service";
 import {ApiUrlService} from "../../shared/api-url.service";
+import {PlatformLocation} from "@angular/common";
+import {GlobalEventManagementService} from "../../core/global-event-management.service";
 
 @Injectable()
 export class CanActivateAuthGuardService implements CanActivate, CanActivateChild {
 
+  private LOGIN_PATH: string = "/staff-ui/login";
+
   constructor(private apiUrlService: ApiUrlService,
               private authorizationService: AuthorizationService,
-              private utilityService: UtilityService) {
+              private utilityService: UtilityService,
+              private location: PlatformLocation,
+              private globalEventManagementService: GlobalEventManagementService) {
+
+    location.onPopState(() => {
+      if(window.location.pathname === this.LOGIN_PATH){
+        this.globalEventManagementService.setShowHeader(false);
+        sessionStorage.clear();
+      }
+    });
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
