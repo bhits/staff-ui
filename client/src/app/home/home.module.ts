56526656bf26834e44b277efb1a5fcf829737e87
Teng Luo <Teng.Luo@feisystems.com>
2017-10-11 16:59:40 -0400

    Implement demo warning dialog UI on  staff ui
    #14238: As a user, I will be notified when working in a demo system with a notice of disclosure, so that I am reminded of PII constraints.

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,12 +3,13 @@ import {HomeComponent} from "./home/home.component";
 import {SharedModule} from "../shared/shared.module";
 import {HomeRoutingModule} from "./home-routing.module";
 import {TranslateModule} from "@ngx-translate/core/index";
-
+import {FormsModule} from "@angular/forms";
 @NgModule({
   imports: [
     SharedModule,
     HomeRoutingModule,
-    TranslateModule
+    TranslateModule,
+    FormsModule
   ],
   declarations: [HomeComponent]
 })
--- client/src/app/home/home/home.component.ts
+++ client/src/app/home/home/home.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {ApiUrlService} from "../../shared/api-url.service";
 import {UtilityService} from "../../shared/utility.service";
 import {UserService} from "../../user/shared/user.service";
+import {AuthenticationService} from "../../security/shared/authentication.service";
+import {SessionStorageService} from "../../security/shared/session-storage.service";
+import {Md2Dialog, Md2DialogConfig} from "md2/dialog/dialog";
 
 @Component({
   selector: 'c2s-home',
@@ -11,13 +14,24 @@ import {UserService} from "../../user/shared/user.service";
 export class HomeComponent implements OnInit {
   public numberOfUsers: number = 0;
   public patientsMapping:any;
+  @ViewChild('warningDialog') warningDialog: Md2Dialog;
+  private readonly DEMO_DISCLAIMER_DISABLED: string = 'demoDisclaimerDisabled';
+  public disabled : boolean =false;
 
   constructor(private apiUrlService: ApiUrlService,
               private userService: UserService,
-              private utilityService: UtilityService) {
+              private utilityService: UtilityService,
+              private authenticationService: AuthenticationService,
+              private sessionStorageService: SessionStorageService,
+              ) {
   }
 
   ngOnInit() {
+    let config = new Md2DialogConfig();
+    config.disableClose = true;
+    this.warningDialog.open(config);
+
+
     const FIRST_PAGE: number = 0;
     this.userService.getUsers(FIRST_PAGE)
       .subscribe(
@@ -38,4 +52,15 @@ export class HomeComponent implements OnInit {
   navigateTo() {
     this.utilityService.navigateTo(this.apiUrlService.getUserListUrl());
   }
+
+  public continue(dialog: any): void {
+    dialog.close();
+    this.sessionStorageService.store(this.DEMO_DISCLAIMER_DISABLED,true);
+  }
+
+  public logout(dialog: any): void {
+    dialog.close();
+    this.authenticationService.logout();
+  }
+
 }
