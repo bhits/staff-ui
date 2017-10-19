bbf6cd9d22ec5dccdc42e5c8831bc00e01fefe52
Teng Luo <Teng.Luo@feisystems.com>
2017-10-19 15:07:05 -0400

    Move the FormModule to the ShareModule
    #14238-notice-dialog

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,13 +3,12 @@ import {HomeComponent} from "./home/home.component";
 import {SharedModule} from "../shared/shared.module";
 import {HomeRoutingModule} from "./home-routing.module";
 import {TranslateModule} from "@ngx-translate/core/index";
-import {FormsModule} from "@angular/forms";
+
 @NgModule({
   imports: [
     SharedModule,
     HomeRoutingModule,
     TranslateModule,
-    FormsModule
   ],
   declarations: [HomeComponent]
 })
